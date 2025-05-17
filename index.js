31704a14ff2eef33cc05d2a18f94ab9fab0bc1b5
PanTsir1 <panagiotistsifetakis@hotmail.com>
2025-05-17 23:20:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
-let rooms = {};
+let waitingPlayer = null;
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
+  console.log('A user connected:', socket.id);
 
-  socket.on('joinGame', () => {
-    let room = findOrCreateRoom();
+  if (waitingPlayer) {
+    const room = `room-${waitingPlayer.id}-${socket.id}`;
     socket.join(room);
-    socket.emit('joinedRoom', room);
+    waitingPlayer.join(room);
 
-    if (rooms[room].length === 2) {
-      io.to(room).emit('startGame', { color: 'white' }); // white always starts
-    }
-  });
+    waitingPlayer.emit('init', 'white');
+    socket.emit('init', 'black');
 
-  socket.on('move', ({ room, move }) => {
-    socket.to(room).emit('opponentMove', move);
-  });
+    waitingPlayer.room = room;
+    socket.room = room;
 
-  socket.on('disconnecting', () => {
-    const roomsLeft = Array.from(socket.rooms).filter(r => r !== socket.id);
-    roomsLeft.forEach(room => {
-      rooms[room] = rooms[room]?.filter(id => id !== socket.id);
-      if (rooms[room]?.length === 0) delete rooms[room];
-    });
-  });
-});
+    console.log(`Players paired in room: ${room}`);
 
-function findOrCreateRoom() {
-  for (const room in rooms) {
-    if (rooms[room].length === 1) {
-      rooms[room].push(socket.id);
-      return room;
-    }
+    waitingPlayer = null;
+  } else {
+    waitingPlayer = socket;
   }
-  const roomId = `room-${Math.random().toString(36).substr(2, 9)}`;
-  rooms[roomId] = [socket.id];
-  return roomId;
-}
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server running');
+  socket.on('move', (move) => {
+    const room = socket.room;
+    if (room) {
+      socket.to(room).emit('move', move);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    if (waitingPlayer === socket) {
+      waitingPlayer = null;
+    }
+    console.log('User disconnected:', socket.id);
+  });
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
