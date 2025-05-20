446b4b45bde5a70c9f708f32b75b931683976367
PanTsir1 <panagiotistsifetakis@hotmail.com>
2025-05-20 12:59:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const io = new Server(server, {
   cors: { origin: '*' }
 });
 
-let waitingPlayer = null;
-const queues = {};
+const queues = {}; // Matchmaking queues by time control
 
 io.on('connection', (socket) => {
-  socket.on('startGame', ({time, increment}) => {
+  socket.on('register', (username) => {
+    socket.data.username = username;
+  });
+
+  socket.on('startGame', ({ time, increment }) => {
     const key = `${time}+${increment}`;
     if (!queues[key]) queues[key] = [];
     queues[key].push(socket);
@@ -21,72 +24,47 @@ io.on('connection', (socket) => {
       const player1 = queues[key].shift();
       const player2 = queues[key].shift();
 
-  socket.on('register', (name) => {
-    socket.data.username = name;
+      const room = `room-${player1.id}-${player2.id}`;
+      player1.join(room);
+      player2.join(room);
 
-    if (waitingPlayer) {
-      const room = `room-${waitingPlayer.id}-${socket.id}`;
-      socket.join(room);
-      waitingPlayer.join(room);
+      const [whiteSocket, blackSocket] = Math.random() < 0.5
+        ? [player1, player2]
+        : [player2, player1];
 
-      const [whiteSocket, blackSocket] = Math.random() < 0.5 ? [waitingPlayer, socket] : [socket, waitingPlayer];
+      whiteSocket.emit('init', {
+        color: 'white',
+        opponent: blackSocket.data.username || 'Opponent',
+        room
+      });
 
-
-      whiteSocket.emit('init', { color: 'white', opponent: blackSocket.data.username });
-      blackSocket.emit('init', { color: 'black', opponent: whiteSocket.data.username });
+      blackSocket.emit('init', {
+        color: 'black',
+        opponent: whiteSocket.data.username || 'Opponent',
+        room
+      });
 
       whiteSocket.room = room;
       blackSocket.room = room;
 
-            // Store opponent info on socket for future moves/messages
-      player1.opponent = player2;
-      player2.opponent = player1;
+      whiteSocket.opponent = blackSocket;
+      blackSocket.opponent = whiteSocket;
     } else {
       socket.emit('waitingForOpponent');
     }
   });
-      waitingPlayer = null;
-    } else {
-      waitingPlayer = socket;
-    }
-  });
 
   socket.on('move', (move) => {
-    if (socket.room) socket.to(socket.room).emit('move', move);
-  });
-
-  socket.on('resign', (color) => {
-    if (socket.room) io.to(socket.room).emit('resign', color);
-  });
-
-  socket.on('drawRequest', () => {
-    if (socket.room) socket.to(socket.room).emit('drawRequest');
-  });
-
-  socket.on('drawAccepted', () => {
-    if (socket.room) io.to(socket.room).emit('drawAccepted');
-  });
-
-  socket.on('rematchRequest', () => {
-    if (socket.room) socket.to(socket.room).emit('rematchRequest');
-  });
-
-  socket.on('rematchAccepted', () => {
-    if (socket.room) io.to(socket.room).emit('rematchAccepted');
-  });
-
-  socket.on('disconnect', () => {
-    for (const key in queues) {
-      queues[key] = queues[key].filter(s => s !== socket);
-    }
-    if (socket.opponent) {
-      socket.opponent.emit('opponentDisconnected');
-      socket.opponent.opponent = null;
+    if (socket.room) {
+      socket.to(socket.room).emit('move', move);
     }
   });
-});
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+
+  socket.on('resign', () => {
+    if (socket.room) {
+      io.to(socket.room).emit('resigned');
+    }
+  });
+
+  socket.on('offerDraw', () => {
 
